0a9f4cb317520c1ec283eaa826c4cc0886d1d5bc
seemeen3900 <seemeen3900@gmail.com>
2017-12-27 19:44:01 -0500

    First_Commit

---
+++ src/main/java/PracticeHome/Home_Practice/App.java
@@ -0,0 +1,13 @@
+package PracticeHome.Home_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
